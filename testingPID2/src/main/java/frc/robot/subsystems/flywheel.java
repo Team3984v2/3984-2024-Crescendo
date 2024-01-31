6a34c7648b4e460079f76f6a18c62043ba03da07
RyanZheng <rzheng1232@gmail.com>
2024-01-31 16:34:28 -0500

    Finished up flywheel code for now

--- testingPID2/src/main/java/frc/robot/subsystems/flywheel.java
+++ testingPID2/src/main/java/frc/robot/subsystems/flywheel.java
@@ -1,27 +1,16 @@
 package frc.robot.subsystems;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-
-import org.opencv.core.Mat;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.SparkMaxRelativeEncoder;
 import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.Constants.Swerve.arm;
-import frc.robot.Constants.Swerve.arm.Shoulder;
-import frc.robot.Constants.Swerve.flywheel.FWtop;
 
 
 public class flywheel extends SubsystemBase{
@@ -39,10 +28,12 @@ public class flywheel extends SubsystemBase{
             SparkMaxRelativeEncoder.Type.kHallSensor, 
             42
         );
+        topEncoder.setVelocityConversionFactor(0);
         bottEncoder = bott.getEncoder(
             SparkMaxRelativeEncoder.Type.kHallSensor, 
             42
         );
+        bottEncoder.setVelocityConversionFactor(0);
         
         FWtopPID = top.getPIDController();
         FWtopPID.setP(Constants.Swerve.flywheel.FWtop.kP);
@@ -53,6 +44,9 @@ public class flywheel extends SubsystemBase{
         FWbottPID.setP(Constants.Swerve.flywheel.FWbott.kP);
         FWbottPID.setI(Constants.Swerve.flywheel.FWbott.kI);
         FWbottPID.setD(Constants.Swerve.flywheel.FWbott.kD);
+
+        top.burnFlash();
+        bott.burnFlash();
     }
 
     public double[] getVelocity(){
@@ -69,16 +63,16 @@ public class flywheel extends SubsystemBase{
         };
         return error;
     }
-    public void GoTo(Rotation2d ShoulderGoal, Rotation2d JointGoal){
+    public void GoTo(double ShoulderGoal, double JointGoal){
         FWtopPID.setReference(
-            ShoulderGoal.getDegrees(), 
+            ShoulderGoal, 
             ControlType.kPosition, 0
             /*ShoulderFF.calculate(ShoulderGoal.getRadians() 
             /* subtract angle offset from horizontal position later *,
              0)*/
         );
         FWbottPID.setReference(
-            JointGoal.getDegrees(), 
+            JointGoal, 
             ControlType.kPosition, 0
             /*ShoulderFF.calculate(ShoulderGoal.getRadians() 
             /* subtract angle offset from horizontal position later *,
@@ -86,20 +80,19 @@ public class flywheel extends SubsystemBase{
         );
     }
 
-    public Command moveTo(double Sangle, double Jangle){
-        Rotation2d[] a = new Rotation2d[]{Rotation2d.fromDegrees(Sangle)/*(49.26)/*getAngles(x, y)[0]*/, Rotation2d.fromDegrees(Jangle)};/*(61.97)};// getAngles(x, y)[1]};*/
+    public Command moveTo(double speedhi, double speedlo){
+        double[] v = new double[]{speedhi, speedlo};
         return runOnce(()->{
-            SmartDashboard.putNumber("Shoulder Goal", a[0].getDegrees());
-            SmartDashboard.putNumber("Joint Goal", a[1].getDegrees());
+            SmartDashboard.putNumber("TopVelocity", v[0]);
+            SmartDashboard.putNumber("BottomVelocity", v[1]);
         }).andThen(run(
             () -> GoTo(
-                a[0], a[1]
-            )
+                v[0], v[1]
+            ))
         ).until(
             ()->(
-                Math.abs(getErrors(a)[0].getDegrees()) < 1 
-                && Math.abs(getErrors(a)[1].getDegrees()) < 1
-            ))
+                Math.abs(getErrors(v)[0])<1 && Math.abs(getErrors(v)[1]) < 1
+            )
         );
     }
     public void periodic(){
